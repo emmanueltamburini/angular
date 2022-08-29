d50f04d3f6b61c509ad595b623561ea78508bcb2
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2022-08-29 16:55:02 -0400

    62. @Input

--- 01-bases/src/app/dbz/characters/characters.component.ts
+++ 01-bases/src/app/dbz/characters/characters.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Character } from '../interfaces/dbz.interface';
 
 @Component({
   selector: 'app-characters',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
 })
 export class CharactersComponent {
 
-  characters: any[] = [];
+  @Input('characters')
+  characters: Character[] = [];
 }
