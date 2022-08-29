966c3210422dccc34116c86d100ea126c05279d1
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2022-08-29 16:24:29 -0400

    59. ngModel

--- 01-bases/src/app/dbz/main-page/main-page.component.ts
+++ 01-bases/src/app/dbz/main-page/main-page.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 
+interface Character {
+  name: string;
+  power: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent {
 
-  public add (): void {
-    console.log('pruebas');
+  public newCharacter: Character = {
+    name: 'Trunks',
+    power: 14000
   }
+
+  public add (): void {
+    console.log(this.newCharacter);
+  }
+
+/*   public changeName(event: any) : void {
+    console.log(event.target.value)
+  } */
 }
