58387e5d2e21246b686bdd711142d38f158059ec
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2022-10-02 21:28:28 -0400

    lifecycles app has been done

--- 07-lifecycle-app/src/app/app.component.ts
+++ 07-lifecycle-app/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lifecycles';
+
+  public show: boolean = true;
+
+  public onHide(): void {
+    this.show = !this.show;
+  }
 }
--- 07-lifecycle-app/src/app/app.module.ts
+++ 07-lifecycle-app/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './pages/principal/principal.component';
+import { ShowNameComponent } from './components/show-name/show-name.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent,
+    ShowNameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
